83d227ee592506f82f95b1ced4419b530aeb11f6
shweta.mathur <shweta.mathur@servicenow.com>
2020-01-20 01:39:28 +0530

    added module

--- main.tf
+++ main.tf
@@ -5,10 +5,30 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
+#module "simple" {
+#    prefix		= "testcase1"
+#    source              = "github.com/iramathur1991/terraform-azurerm-simple"
+#    region              = "westus"
+#   
+#  }
+//--------------------------------------------------------------------
+// Variables
+variable "demoinfra_clientId" {}
+variable "demoinfra_clientSecret" {}
+variable "demoinfra_region" {}
+variable "demoinfra_subscriptionId" {}
+variable "demoinfra_tenantId" {}
 
-module "simple" {
-    prefix		= "testcase1"
-    source              = "github.com/iramathur1991/terraform-azurerm-simple"
-    region              = "westus"
-   
-  }
+//--------------------------------------------------------------------
+// Modules
+module "demoinfra" {
+  source  = "app.terraform.io/cmporg/demoinfra/azure"
+  version = "1.0.1"
+
+  clientId = "${var.demoinfra_clientId}"
+  clientSecret = "${var.demoinfra_clientSecret}"
+  prefix = "qwerty"
+  region = "${var.demoinfra_region}"
+  subscriptionId = "${var.demoinfra_subscriptionId}"
+  tenantId = "${var.demoinfra_tenantId}"
+}
