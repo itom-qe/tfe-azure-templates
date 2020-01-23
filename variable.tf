1ea1157c4dcf9419d9ad3a2ca6a5a0961ac85be8
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-01-23 18:08:00 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,39 +1,50 @@
+variable "subscriptionId" {
+}
 
-variable "resource_group" "rg"{
+variable "clientId" {
+}
+
+variable "clientSecret" {
+}
+
+variable "tenantId" {
+}
+
+variable "rgName" {
   description = "The name of the resource group in which to create the virtual network."
-  default="terraform-group"
+  default     = "terraform-group"
 }
 
 variable "rg_prefix" {
   description = "The shortened abbreviation to represent your resource group that will go on the front of some resources."
-  default     = "rg"
+  default     = "azuretg"
 }
 
 variable "hostname" {
   description = "VM name referenced also in storage-related names."
-  default="tf"
+  default     = "tf"
 }
 
 variable "dns_name" {
   description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
-  default="vm"
+  default     = "vm"
 }
 
 variable "lb_ip_dns_name" {
   description = "DNS for Load Balancer IP"
-  default="lb1"
+  default     = "lb1"
 }
 
-variable "vm_count_per_subnet" "subnet"{
-	description = "count per subnet"
-	default=2
+variable "vm_count_per_subnet" {
+  description = "count per subnet"
+  default     = 2
 }
 
 variable "region" {
   description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
 }
 
-variable "virtual_network_name" "vnet"{
+variable "network" {
   description = "The name for the virtual network."
   default     = "vnet"
 }
@@ -42,7 +53,6 @@ variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
 }
-
 variable "subnet_prefix" {
   description = "The address prefix to use for the subnet."
   default     = "10.0.10.0/24"
@@ -57,8 +67,12 @@ variable "storage_replication_type" {
   description = "Defines the Replication Type to use for this storage account. Valid options include LRS, GRS etc."
   default     = "LRS"
 }
+variable "subnet"{
+description = "Defines the subnetname"
+default = "sbnet"
+}
 
-variable "vm_size" "vm_size" {
+variable "vm_size" {
   description = "Specifies the size of the virtual machine."
   default     = "Standard_D1_v2"
 }
@@ -87,8 +101,7 @@ variable "admin_username" {
   description = "administrator user name"
   default     = "vmadmin"
 }
-
 variable "admin_password" {
   description = "administrator password (recommended to disable password auth)"
-  default = "admin01!"
+  default     = "admin01!"
 }
