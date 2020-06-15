52f9575ca187691b1110cb484b38d84b4e7ea692
shweta.mathur <shweta.mathur@servicenow.com>
2020-06-15 19:59:25 +0530

    emove

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
   client_id       = var.clientId
   client_secret   = var.clientSecret
   tenant_id       = var.tenantId
-  version         = "=2.0.0"
+  #version         = "=2.0.0"
   features {}
 }
