bc1cbbb78902699f216b646dd4fdb4aa74dffd55
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-06-15 19:10:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "azurerm_virtual_machine" "vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   availability_set_id   = azurerm_availability_set.avset.id
   vm_size               = var.vm_size
-  network_interface_ids = ["${element(azurerm_network_interface.nic.*.id, count.index)}"]
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
   count                 = var.vm_count_per_subnet
 
   storage_image_reference {
